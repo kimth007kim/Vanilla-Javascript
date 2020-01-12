80b577e72d65ba42fa14bb643117d6e2b947d5f6
kimth007kim <kimth007kim@naver.com>
2020-01-13 00:37:13 +0900

    #3.7 Making a To Do List Part Three 완성

--- todo.js
+++ todo.js
@@ -4,14 +4,23 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+function filterFn(toDo) {
+  return toDo.id === 1;
+
+}
+
+let toDos = [];
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-
-
+  const cleanToDos = toDos.filter(function (toDo) {
+    console.log(toDo.id, li.id);
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos;
 }
 
 function saveToDos() {
