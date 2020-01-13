a53a329f792718c3d5066bcd60a083e8c68ea552
kimth007kim <kimth007kim@naver.com>
2020-01-14 01:55:30 +0900

    #3.10 Getting the Weather part Two(API) Got some errors

--- weather.js
+++ weather.js
@@ -1,5 +1,20 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "eeccb44cb115403ac5627f6c9aaa9907 ";
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place} `;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +28,7 @@ function handleGeoSuccess(position) {
     longitude: longitude
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 function handleGeoError() {
   console.log("Cant access geo location");
@@ -23,10 +39,12 @@ function askForCoords() {
 }
 
 function loadCoords() {
-  const loadedCords = localStorage.getItem(COORDS);
-  if (loadedCords === null) {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
